61cb678e3e140a9bdb217668c8a19c37a41ee4f3
dariuszporebski79 <dariusz.porebski79@wp.pl>
2023-04-01 17:10:43 +0200

    ok_version

--- Numbers_Guessing_Game.py
+++ Numbers_Guessing_Game.py
@@ -1,12 +1,14 @@
 from random import randint
 computer_number = randint(1, 100)
 while True:
-    player_number = int(input("Guess the number: "))
-    if player_number < computer_number:
-        print("Too small!")
-    elif player_number > computer_number:
-        print("Too big!")
-    else:
-        print("You win")
-        break
-# ValueError
+    try:
+        player_number = int(input("Guess the number: "))
+        if player_number < computer_number:
+            print("Too small!")
+        elif player_number > computer_number:
+            print("Too big!")
+        else:
+            print("You win")
+            break
+    except ValueError:
+        print("It's not a number!")
